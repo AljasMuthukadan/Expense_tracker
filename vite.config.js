3292ab992c423d9b9041328444904e897a99a786
Aljas M <aljasmuthukadan@gmail.com>
2025-09-02 22:12:26 +0530

    tried to correct again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from  '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Expense-Tracker/',
+  base: '/Expense_tracker/',
 })
