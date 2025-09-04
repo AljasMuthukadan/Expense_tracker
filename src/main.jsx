66a448fd174b05f143d81e29fd5fa4e2c365e65a
Aljas M <aljasmuthukadan@gmail.com>
2025-09-04 21:44:01 +0530

    alaj

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/expense_tracker">
       <App />
     </HashRouter>
   </StrictMode>
